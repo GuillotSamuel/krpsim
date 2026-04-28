5ebab5abc5c47c45ee6ee8709e1532f0fb90c4dd
Samuel_Guillot <samuelguillot75@gmail.com>
2026-04-28 10:30:04 +0200

    chore (config.py): changing the default configuration

--- src/config.py
+++ src/config.py
@@ -2,9 +2,9 @@
 #   "unlimited"  — no restriction, any number of instances can run simultaneously
 #   "per_tick"   — each process can be started at most MAX_PARALLEL times per scheduling round
 #   "concurrent" — each process can have at most MAX_PARALLEL instances running at the same time
-PARALLEL_MODE: str = "per_tick"
+PARALLEL_MODE: str = "concurrent"
 MAX_PARALLEL:  int = 1
 # Advande time modes:
 #   "+1"            — Jump the clock forward of one tick
 #   "process_end"   — Jump the clock forward at the time of the first end of the current process
-TICK_MODE: str = "+1"
+TICK_MODE: str = "process_end"
